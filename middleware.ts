bd340902caa616a034f25ccdcc50332e7766133b
SnigdhaSucharita <snigdhasucharita7@gmail.com>
2026-01-28 18:48:50 +0530

    small fix

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,7 @@ export function middleware(request: NextRequest) {
     "/signup",
     "/forgot-password",
     "/reset-password",
+    "/verify-email",
   ];
 
   const isPublicRoute = publicRoutes.some(
