820ad27c37337700515c6ac4ea4ba37908e4df9f
SnigdhaSucharita <snigdhasucharita7@gmail.com>
2026-01-31 01:01:40 +0530

    cookie

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest } from "next/server";
 import { proxyRequest } from "../../_proxy";
+import { cookies } from "next/headers";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
+  cookies();
   return proxyRequest(req, "/api/auth/me");
 }
